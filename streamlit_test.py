148a374ca5b3c2d9e1dd7e10a4690e09f2f29704
song20023490 <117272941+song20023490@users.noreply.github.com>
2024-07-19 17:35:28 +0800

    Add files via upload

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
+st.title('st.secrets')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
-pr = df.profile_report()
-st_profile_report(pr)
+st.write(st.secrets['message'])
