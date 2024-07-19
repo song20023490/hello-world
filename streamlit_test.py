d700227c1219ebe32f6277dada802a21e317bd94
song20023490 <117272941+song20023490@users.noreply.github.com>
2024-07-19 17:27:31 +0800

    Add files via upload

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,39 +1,11 @@
 import streamlit as st
-from datetime import time, datetime
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-st.header('st.slider')
+st.header('`streamlit_pandas_profiling`')
 
-# 样例 1
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-st.subheader('Slider')
-
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
-
-# 样例 2
-
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-# 样例 3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
-
-# 样例 4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+pr = df.profile_report()
+st_profile_report(pr)
